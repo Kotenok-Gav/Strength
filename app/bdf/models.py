c128d9b928fbcedcd928b4ee5087695fa589c367
Ilya Nefedov <nefedov_ilya96@mail.ru>
2023-05-26 17:14:49 +0500

    Доработка формы_2

--- app/bdf/models.py
+++ app/bdf/models.py
@@ -17,6 +17,33 @@ class Rockets_bdf(models.Model):
 
     # 1.3 Время выхода ракеты из контейнера в секундах с точностью до тысячных
     t = models.DecimalField(decimal_places=3, max_digits=6)
+
+    #------------------------------------------------------------------------
+
+    # 2 Геометрические характеристики
+    # 2.1.1 Диаметр ракеты:  м, с точностью до сотых
+    d0 = models.DecimalField(decimal_places=2, max_digits=3)
+
+    #(НЕТ)
+    # 2.1.2 Эквивалентная толщина стенки ракеты   м, с точностью до тысячных
+    tol_R = models.DecimalField(decimal_places=3, max_digits=4)
+
+    # 2.1.3 Длина ракеты:  м, с точностью до десятых
+    L = models.DecimalField(decimal_places=1, max_digits=4)
+
+    #(НЕТ)
+    # 2.2.1 Диаметр контейнера:  м, с точностью до сотых
+    d0_Kon = models.DecimalField(decimal_places=2, max_digits=3)
+
+    #(НЕТ)
+    # 2.2.2 Эквивалентная толщина стенки контейнера   м, с точностью до тысячных
+    tol_Kon = models.DecimalField(decimal_places=3, max_digits=4)
+
+    #(НЕТ)
+    # 2.2.3 Длина контейнера:  м, с точностью до десятых
+    L_Kon = models.DecimalField(decimal_places=1, max_digits=4)
+
+    #------------------------------------------------------------------------
     
 
     
--- frontend/src/pages/Forms.js
+++ frontend/src/pages/Forms.js
@@ -4,9 +4,16 @@ import Tool from "../components/input/Tooltip";
 
 function Forms() {
   const [bdf, setBdf] = useState([]);
-  const [text, setText] = useState("");
+
+  const [text, setText] = useState();
   const [start_rocket, setStart_rocket] = useState();
   const [t, sett] = useState();
+  const [d0, setd0] = useState();
+  const [tol_R, settol_R] = useState();
+  const [L, setL] = useState();
+  const [d0_Kon, setd0_Kon] = useState();
+  const [tol_Kon, settol_Kon] = useState();
+  const [L_Kon, setL_Kon] = useState();
 
   const addBdfHandler = () => {
     const postBdf = async () => {
@@ -14,6 +21,12 @@ function Forms() {
         text: text,
         start_rocket: start_rocket,
         t: t,
+        d0:d0,
+        tol_R:tol_R,
+        L:L,
+        d0_Kon:d0_Kon,
+        tol_Kon:tol_Kon,
+        L_Kon:L_Kon
       };
       const { data } = await axios.post(
         "http://127.0.0.1:8000/bdf/",
@@ -23,6 +36,12 @@ function Forms() {
       setText("");
       setStart_rocket("");
       sett("");
+      setd0("");
+      settol_R("");
+      setL("");
+      setd0_Kon("");
+      settol_Kon("");
+      setL_Kon("");
     };
     postBdf();
   };
@@ -42,8 +61,8 @@ function Forms() {
           <h1 className="text-3xl text-center pb-5">BDF файл</h1>
 
           <p className="pb-3 pl-5 text-2xl">1  Общие данные</p>
-
-          {/* 1 */}
+          {/* ------------------------------------------------------------------ */}
+          {/* 1.1 */}
           <p className="pb-3 pl-5 text-xl">1.1  Название проекта</p>
           <div className="flex items-center justify-between bg-slate-700 rounded-3xl px-5 ">
             <input
@@ -58,7 +77,7 @@ function Forms() {
 
           <br></br>
 
-          {/* 2 */}
+          {/* 1.2 */}
           <p className="pb-3 pl-5 text-xl">1.2 Определение старта ракеты</p>
           <div className="flex items-center justify-between bg-slate-700 rounded-3xl px-5 ">
             <input
@@ -73,7 +92,7 @@ function Forms() {
 
           <br></br>
 
-          {/* 3 */}
+          {/* 1.3 */}
           <p className="pb-3 pl-5 text-xl">1.3 Время выхода ракеты из контейнера в секундах </p>
           <div className="flex items-center justify-between bg-slate-700 rounded-3xl px-5 ">
             <input
@@ -83,10 +102,127 @@ function Forms() {
               className="w-full py-2 rounded-xl bg-slate-700 text-white outline-none"
               placeholder="5.673"
             />
-            <Tool text="С точностью до тысячных"></Tool>
+            <Tool text="c точностью до тысячных"></Tool>
           </div>
 
           <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+
+
+          <p className="pb-3 pl-5 text-2xl">2  Геометрические характеристики</p>
+          {/* ------------------------------------------------------------------ */}
+          {/* 2.1.1 */}
+          <p className="pb-3 pl-5 text-xl">2.1.1 Диаметр ракеты</p>
+          <div className="flex items-center justify-between bg-slate-700 rounded-3xl px-5 ">
+            <input
+              value={d0}
+              onChange={e => setd0(e.target.value)}
+              type="text"
+              className="w-full py-2 rounded-xl bg-slate-700 text-white outline-none"
+              placeholder="3.03"
+            />
+            <Tool text="м, с точностью до сотых"></Tool>
+          </div>
+
+          <br></br>
+
+          {/* 2.1.2 */}
+          <p className="pb-3 pl-5 text-xl">2.1.2 Эквивалентная толщина стенки ракеты </p>
+          <div className="flex items-center justify-between bg-slate-700 rounded-3xl px-5 ">
+            <input
+              value={tol_R}
+              onChange={e => settol_R(e.target.value)}
+              type="text"
+              className="w-full py-2 rounded-xl bg-slate-700 text-white outline-none"
+              placeholder="0.007"
+            />
+            <Tool text="м, с точностью до тысячных"></Tool>
+          </div>
+
+          <br></br>
+
+          {/* 2.1.3 */}
+          <p className="pb-3 pl-5 text-xl">2.1.3 Длина ракеты</p>
+          <div className="flex items-center justify-between bg-slate-700 rounded-3xl px-5 ">
+            <input
+              value={L}
+              onChange={e => setL(e.target.value)}
+              type="text"
+              className="w-full py-2 rounded-xl bg-slate-700 text-white outline-none"
+              placeholder="21.6"
+            />
+            <Tool text="м, с точностью до десятых"></Tool>
+          </div>
+
+          <br></br>
+
+          {/* 2.2.1 */}
+          <p className="pb-3 pl-5 text-xl">2.2.1 Диаметр контейнера</p>
+          <div className="flex items-center justify-between bg-slate-700 rounded-3xl px-5 ">
+            <input
+              value={d0_Kon}
+              onChange={e => setd0_Kon(e.target.value)}
+              type="text"
+              className="w-full py-2 rounded-xl bg-slate-700 text-white outline-none"
+              placeholder="3.25"
+            />
+            <Tool text="м, с точностью до сотых"></Tool>
+          </div>
+
+          <br></br>
+
+          {/* 2.2.2 */}
+          <p className="pb-3 pl-5 text-xl">2.2.2 Эквивалентная толщина стенки контейнера </p>
+          <div className="flex items-center justify-between bg-slate-700 rounded-3xl px-5 ">
+            <input
+              value={tol_Kon}
+              onChange={e => settol_Kon(e.target.value)}
+              type="text"
+              className="w-full py-2 rounded-xl bg-slate-700 text-white outline-none"
+              placeholder="0.004"
+            />
+            <Tool text="м, с точностью до тысячных"></Tool>
+          </div>
+
+          <br></br>
+
+          {/* 2.2.3 */}
+          <p className="pb-3 pl-5 text-xl">2.2.3 Длина контейнера</p>
+          <div className="flex items-center justify-between bg-slate-700 rounded-3xl px-5 ">
+            <input
+              value={L_Kon}
+              onChange={e => setL_Kon(e.target.value)}
+              type="text"
+              className="w-full py-2 rounded-xl bg-slate-700 text-white outline-none"
+              placeholder="25.0"
+            />
+            <Tool text="м, с точностью до десятых"></Tool>
+          </div>
+
+          <br></br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
           <button
                 onClick={addBdfHandler}
@@ -105,6 +241,12 @@ function Forms() {
                 <p>{bdf.text}</p>
                 <p>{bdf.start_rocket}</p>
                 <p>{bdf.t}</p>
+                <p>{bdf.d0}</p>
+                <p>{bdf.tol_R}</p>
+                <p>{bdf.L}</p>
+                <p>{bdf.d0_Kon}</p>
+                <p>{bdf.tol_Kon}</p>
+                <p>{bdf.L_Kon}</p>
               </div>
             ))}
           </div>
